c1a8c0bf9c61c179191f455ae3ce0ff0a49d13b9
MikeTheCoder <93738693+MikeNcube@users.noreply.github.com>
2022-03-07 22:36:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function(window) {
+    const helloSpeaker ={};
+const helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+const greeting = "Hello";
+helloSpeaker.sayHello = function () {
+    console.log(greeting + helloSpeaker.names)
+}
+}
+
+
+a();
